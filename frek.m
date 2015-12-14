2112d8c4b4f26849afb01acbfa21a0742d192973
FulyaSariyar <fulya12agustos@hotmail.com>
2015-12-15 00:36:29 +0200

    frek.m dosyasını güncelledim.

--- frek.m
+++ frek.m
@@ -1,6 +1,6 @@
 function [ frekans ] = frek( nota, oktav )
-f0=440; %Temel frekans(referans) deðeri La4
-switch nota 
+f0=440; %Temel frekans(referans) degeri La4
+switch nota %girilen notaya göre deger (Örn: Do 1, C# 2, vb..)
         case 'Do'
         Nota=1;
         case 'C#'
@@ -26,8 +26,9 @@ switch nota
         case 'Si'
         Nota=12;
     otherwise 
-        Nota=10;
+        Nota=NULL;%girilen nota yoksa NULL döndürür.
         
 end
-frekans=f0*2^((oktav-4)+(Nota-10)/12);
+frekans=f0*2^((oktav-4)+(Nota-10)/12);%nota tablosundaki frekanslar arasý iliþkinin matematiksel formülüdür.
+                                      %f0=440 temel frekansdir.
 end
