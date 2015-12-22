1ea8e7421ac0c97578f8c1c61a1bf53e96625add
FulyaSariyar <fulya12agustos@hotmail.com>
2015-12-22 11:41:38 +0200

    note.m dosyasını güncelledim.

--- note.m
+++ note.m
@@ -1,6 +1,14 @@
-function [y] = note( altsinir, ustsinir )%ciktisi y olan ve altsinir, ustsinir argümanlarini alan note adindaki fonksiyon.
-tt=altsinir:(ustsinir-altsinir)/100:ustsinir;%üstsinir ve altsinir arasinda degisen bir vektör.Artis miktari 'ustsinir-altsinir' degerinin yüzde biridir. 
-y=sin(tt);%y ciktisi bir sinüs sinyalidir.
-plot(y);%sinyalin grafigini çizer.
-end
+function [xx,tt]=note(frekans,vurus)
+fs=8192;
+tt=0:1/fs:vurus;
+xx=sin(2*pi*frekans*tt);
+t1=0:1/fs:2*vurus/8;
+x1=3/2*sin(2*pi*frekans*t1)
+t2=2*vurus/8:1/fs:3*vurus/8
+x2=2/3*sin(2*pi*frekans*t2)
+t3=3*vurus/8:1/fs:7*vurus/8
+x3=1
+t4=7*vurus/8:1/fs:8*vurus/8
+x4=sin(2*pi*frekans*t4)
 
+end
