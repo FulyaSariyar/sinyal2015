a86a4b2e2fe420254a06a86d436262d924395844
FulyaSariyar <fulya12agustos@hotmail.com>
2015-12-28 19:37:34 +0200

    frek.m dosyasını yeniden düzenledim.

--- frek.m
+++ frek.m
@@ -1,30 +1,38 @@
 function [ frekans ] = frek( nota, oktav )
 f0=440; %Temel frekans(referans) degeri La4
+if nargin == 1 
+    oktav = '4';% oktav degeri verilmemisse 4. oktav kabul eder.
+end
 switch nota %girilen notaya göre deger (Örn: Do 1, C# 2, vb..)
         case 'Do'
         Nota=1;
-        case 'C#'
+        case 'Dod'
         Nota=2;
         case 'Re'
         Nota=3;
-        case 'Eb'
+        case 'Mib'
         Nota=4;
         case 'Mi'
         Nota=5;
         case 'Fa'
         Nota=6;
-        case 'F#'
+        case 'Fad'
         Nota=7;
         case 'Sol'
         Nota=8;
-        case 'G#'
+        case 'Sold'
         Nota=9;
         case 'La'
         Nota=10;
-        case 'Bb'
+        case 'Sib'
         Nota=11;
         case 'Si'
         Nota=12;
+        case 'sus'
+        Nota=13;
+        if Nota==13
+            frekans=NULL;
+        end
     otherwise 
         Nota=NULL;%girilen nota yoksa NULL döndürür.
         
