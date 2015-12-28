b2024b73922727ca639bbeacf2424bf33f05bdf3
FulyaSariyar <fulya12agustos@hotmail.com>
2015-12-29 01:27:38 +0200

    note.m dosyasını yeniden düzenledim.

--- note.m
+++ note.m
@@ -1,6 +1,7 @@
-function [xx,tt] = note(frekans,vurus)
+function [h,tt] = note(frekans,vurus)
     fs=8192;%ornekleme frekansi
     tt=0:1/fs:vurus; 
+    x=sin(2*pi*frekans*tt);
     
     A=[linspace(0,1.5,2*fs*vurus/8),...;%attack süresi
     linspace(1.5,1,fs*vurus/8),...;     %decay süresi   
@@ -8,7 +9,14 @@ function [xx,tt] = note(frekans,vurus)
     linspace(1,0,fs*vurus/8+1)];        %release süresi  
 
     xx=A.*sin(2*pi*frekans*tt);%A vektörü sinüs sinyali ile carpilarak zarfa sokulmus oluyor.
+                               %burada sin(2*pi*frekans*tt)yerine direk x'de yazabilirdim.
+                           
+                               
+                              
                   
-   
-    
+    h1=0.80*sin(2*pi*frekans*tt*2); %1. harmonik (%80)
+    h2=0.40*sin(2*pi*frekans*tt*3); %2. harmonik (%40)
+    h3=0.10*sin(2*pi*frekans*tt*4); %3. harmonik (%10)
+    h=x+h1+h2+h3;
+    plot(tt,h);
 end
