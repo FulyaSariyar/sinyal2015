c7f6d42fbc2c87d72fe117fc23585fa06233abc1
FulyaSariyar <fulya12agustos@hotmail.com>
2016-01-05 01:24:26 +0200

    note.m dosyasını yeniden düzenledim.

--- note.m
+++ note.m
@@ -1,26 +1,22 @@
-function [x,t] = note(frekans,sure)
-   
-    Fs=8192; % ornekleme frekansi
-    Ts=1/Fs;   
-    t=0:Ts:sure; % [0 - sure] sn araliginda,Fs frekansinda ornekler olusturulur
+function [h,tt] = note(frekans,vurus)
+    fs=8192;%ornekleme frekansi
+    tt=0:1/fs:vurus; 
+    x=sin(2*pi*frekans*tt);
     
-    
-    A=[linspace(0,1.5,2*Fs*sure/8),... % 2T/8'lik surede [0 - 1.5] artis
-    linspace(1.5,1,Fs*sure/8),...      % T/8'lik surede [1.5 - 1] azalis
-    linspace(1,1,4*Fs*sure/8),...      % 4T/8'lik surede [1]sabit
-    linspace(1,0,Fs*sure/8+1)];        % T/8'lik surede [1 - 0] azalis
-    
-    h0=sin(2*pi*frekans*t); %orijinal
-    h1=sin(2*pi*frekans*t*2); %harmonik1
-    h2=sin(2*pi*frekans*t*3); %harmonik2
-    h3=sin(2*pi*frekans*t*4); %harmonik3
-    h=h0+0.8*h1+0.4*h2+0.1*h3; % %80.h1 + %40.h2 + %10.h3
-    
-    x=A.*h;
-    %katsayilar vektoru, sinus sinyali ile carpilarak,
-    %zarfa uygun bir cikis sinyali olusturuluyor.
-    %Not: .* kullaniminin sebebi matlabin matris carpimi hatasi vermesini
-    %onlemektir.
-    
-    
+    A=[linspace(0,1.5,2*fs*vurus/8),...;%attack süresi
+    linspace(1.5,1,fs*vurus/8),...;     %decay süresi   
+    linspace(1,1,4*fs*vurus/8),...;     %sustain süresi
+    linspace(1,0,fs*vurus/8+1)];        %release süresi  
+
+    xx=A.*sin(2*pi*frekans*tt);%A vektörü sinüs sinyali ile carpilarak zarfa sokulmus oluyor.
+                               %burada sin(2*pi*frekans*tt)yerine direk x'de yazabilirdim.
+                           
+                               
+                              
+                  
+    h1=0.80*sin(2*pi*frekans*tt*2); %1. harmonik (%80)
+    h2=0.40*sin(2*pi*frekans*tt*3); %2. harmonik (%40)
+    h3=0.10*sin(2*pi*frekans*tt*4); %3. harmonik (%10)
+    h=x+h1+h2+h3;
+    plot(tt,h);
 end
