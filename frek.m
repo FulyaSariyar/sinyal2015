7443db7b07363a42f8fb664702324f4700642dbb
FulyaSariyar <fulya12agustos@hotmail.com>
2016-01-04 22:08:23 +0200

    frek.m dosyasını yeniden düzenledim.

--- frek.m
+++ frek.m
@@ -1,42 +1,20 @@
 function [ frekans ] = frek( nota, oktav )
-f0=440; %Temel frekans(referans) degeri La4
-if nargin == 1; 
-    oktav = '4';% oktav degeri verilmemisse 4. oktav kabul eder.
+notalar={'Do','Dod','Re','Mib','Mi','Fa','Fad','Sol','Sold','La','Sib','Si','sus'}; %notalari tanimladim.
+frekans=0; %frekans sus gelme ihtimaline karsi sifirlandi.
+
+if nargin < 2  % eger oktav belirtilmemisse,
+    oktav = 4; % 4.oktav kabul ediliyor.
 end
-switch nota %girilen notaya göre deger 
-        case 'Do'
-        Nota=1;
-        case 'Dod'
-        Nota=2;
-        case 'Re'
-        Nota=3;
-        case 'Mib'
-        Nota=4;
-        case 'Mi'
-        Nota=5;
-        case 'Fa'
-        Nota=6;
-        case 'Fad'
-        Nota=7;
-        case 'Sol'
-        Nota=8;
-        case 'Sold'
-        Nota=9;
-        case 'La'
-        Nota=10;
-        case 'Sib'
-        Nota=11;
-        case 'Si'
-        Nota=12;
-        case 'sus'
-        Nota=13;
-        if Nota==13
-            frekans=NULL;%Nota degeri 13 oldugu zaman frekans NULL döndürülür.
-        end
-    otherwise 
-        Nota=NULL;%girilen nota yoksa NULL döndürür.
-        
+
+nota=find(ismember(notalar,nota)); 
+%arguman olarak disaridan girilen nota, notalar dizisinde bulunuyorsa
+%kacinci eleman olarak bulunduguna bakilir ve frekans hesabinda
+%kullanilir.
+if nota ~= 13 %eger 13.nota(sus) geldiyse frekans hesaplanmaz.                                        
+    f0 = 440; %Temel frekans(referans) degeri La4
+    frekans=f0*2^((nota-10)/12 + (oktav-4)); 
+    %nota tablosundaki frekanslar arasi iliskinin matematiksel formülüdür.
+    %f0=440 temel frekansdir.
 end
-frekans=f0*2^((oktav-4)+(Nota-10)/12);%nota tablosundaki frekanslar arasi iliskinin matematiksel formülüdür.
-                                      %f0=440 temel frekansdir.
+
 end
